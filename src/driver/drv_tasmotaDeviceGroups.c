591313333ebdd240dc4719747693027fab053c2f
NorthernMan54 <19808920+NorthernMan54@users.noreply.github.com>
2026-06-27 21:24:34 -0400

    Revert "Update drv_tasmotaDeviceGroups.c"
    This reverts commit 25bae6ff30c1d09f4cd08be7c99ba75b6b5b5b12.

--- src/driver/drv_tasmotaDeviceGroups.c
+++ src/driver/drv_tasmotaDeviceGroups.c
@@ -14,26 +14,6 @@
 #include "lwip/inet.h"
 #include "../httpserver/new_http.h"
 
-// Portable FreeRTOS tick-to-milliseconds conversion
-// Works across different FreeRTOS variants (some don't define portTICK_PERIOD_MS)
-#ifndef pdTICKS_TO_MS
-  #if defined(configTICK_RATE_HZ) && (configTICK_RATE_HZ > 0)
-    #define pdTICKS_TO_MS(ticks) ((uint32_t)(((uint64_t)(ticks) * 1000ULL) / (uint64_t)configTICK_RATE_HZ))
-  #else
-    // Conservative fallback if tick rate is unknown
-    #define pdTICKS_TO_MS(ticks) ((uint32_t)(ticks))
-  #endif
-#endif
-
-/* FreeRTOS compatibility for portTICK_PERIOD_MS across ports/toolchains */
-#ifndef portTICK_PERIOD_MS
-  #ifdef portTICK_RATE_MS
-    #define portTICK_PERIOD_MS portTICK_RATE_MS
-  #else
-    #define portTICK_PERIOD_MS (1000 / configTICK_RATE_HZ)
-  #endif
-#endif
-
 static const char* dgr_group = "239.255.250.250";
 static int dgr_port = 4447;
 static int dgr_retry_time_left = 5;
@@ -156,6 +136,7 @@ void DGR_AddToUnicastSendQueue(byte *data, int len, uint32_t target_ip) {
 }
 void DGR_FlushSendQueue() {
 	dgrPacket_t *p;
+	int nbytes;
 	bool taken;
 
 	if (g_mutex == 0)
@@ -179,7 +160,7 @@ void DGR_FlushSendQueue() {
 				dest_addr.sin_addr.s_addr = inet_addr(dgr_group);
 			}
 			g_dgr_stat_sent++;
-			(void)sendto(
+			nbytes = sendto(
 				g_dgr_socket_send,
 			   (const char*) p->buffer,
 				p->length,
@@ -505,7 +486,7 @@ dgrMember_t *findMember() {
 	g_dgrMembers[i].ip = ip;
 	g_dgrMembers[i].lastSeq = 0;
 	g_dgrMembers[i].acked_sequence = 0;
-	g_dgrMembers[i].last_heard_time = pdTICKS_TO_MS(xTaskGetTickCount());  // Current time in ms
+	g_dgrMembers[i].last_heard_time = xTaskGetTickCount() / portTICK_PERIOD_MS;  // Current time in ms
 	return &g_dgrMembers[i];
 }
 
@@ -521,7 +502,7 @@ int DGR_CheckSequence(uint16_t seq) {
 	addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_DGR, "DGR_CheckSequence: argument %i, last %i",(int)seq, (int)m->lastSeq);
 	
 	// Update last heard time - this member is alive
-	m->last_heard_time = pdTICKS_TO_MS(xTaskGetTickCount());
+	m->last_heard_time = xTaskGetTickCount() / portTICK_PERIOD_MS;
 	
 	// make it work past wrap at
 	if((seq > m->lastSeq) || (seq+10 > m->lastSeq+10)) {
@@ -587,7 +568,7 @@ void DRV_DGR_RunEverySecond() {
 		return;
 	}
 
-	now = pdTICKS_TO_MS(xTaskGetTickCount());  // Current time in milliseconds
+	now = xTaskGetTickCount() / portTICK_PERIOD_MS;  // Current time in milliseconds
 	groupName = CFG_DeviceGroups_GetName();
 	if(!groupName || !groupName[0]) {
 		return;  // No group configured
@@ -974,7 +955,6 @@ commandResult_t CMD_DGR_SendRGBCW(const void *context, const char *cmd, const ch
 			tmp[1] = *(c++);
 			tmp[2] = '\0';
 			r = sscanf(tmp, "%x", &val);
-			(void)r; /* r intentionally unused - just consuming sscanf result */
 			rgbcw[i] = val;
 			i++;
 		}
@@ -1054,5 +1034,4 @@ void DRV_DGR_Init()
 	//cmddetail:"examples":""}
 	CMD_RegisterCommand("DGR_SendFixedColor", CMD_DGR_SendFixedColor, NULL);
 }
-
 
